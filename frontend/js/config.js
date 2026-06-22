29e7d497ea7430529c50857e5cea52b0cbdd6b5f
ekkasuks <ekkasuks@gmail.com>
2026-06-22 18:24:35 +0700

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,5 +2,5 @@
  * ตั้งค่า URL ของ Apps Script Web App — แก้ค่านี้หลัง deploy backend
  * (ดูวิธีใน config.example.js)
  */
-window.API_URL = "https://script.google.com/macros/s/AKfycbzWRi4nhAk-3M51XUz9FbMIsz62UvWCBbnRXgYD9ut3H1f3GxBaoGF_hL84gbfE_qiMsQ/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycby30b8NF7p0ovI15HfNPz98ncK0ppcewm6BQWIa5DXbWNvWqJtF94QzUuWc-mIYFYI/exec";
 window.APP_VERSION = "1.0.0";
